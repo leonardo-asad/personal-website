befe71dae44ddbfdb9c2a37d097ca959c45ac55f
Leonardo Asad <leonardo.asad.89@gmail.com>
2025-05-18 17:20:19 +1200

    add the analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Viaoda_Libre } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
